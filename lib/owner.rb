25a39c36c48f935b48643ba630fe379b7c4a7783
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-10-20 14:34:03 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -72,7 +72,7 @@ class Owner
   end
   
   def list_pets
-    "I have #{self.dogs.count} dog(s), and #{@owner.cats.count}."
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count}."
     
   end
     
